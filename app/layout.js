1e346c1468783d6979b1442a1fa66577855e4b3b
Shadrack <shadrackkemei2019@gmail.com>
2026-09-22 19:37:27 +0300

    Refactor RootLayout to include Navbar and Footer components for improved site structure

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,7 @@
 import "./globals.css";
+import Navbar from "@/components/layout/Navbar";
+import Footer from "@/components/layout/Footer";
+
 
 export const metadata = {
   title: "BrightSpark Electricals & Electronics",
@@ -10,7 +13,16 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className="flex min-h-screen flex-col">
+        <Navbar />
+
+        <main className="flex-1">
+          {children}
+        </main>
+
+        <Footer />
+
+      </body>
     </html>
   );
 }
